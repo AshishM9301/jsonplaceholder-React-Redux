6fb00863c04a701298a9fe76286b4824399b93bf
ASHISH KUMAR <47477440+asfafasa@users.noreply.github.com>
2020-06-01 16:55:47 +0530

    Updates

---
+++ precache-manifest.f2c6322f216676736a5c612940922f4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b0fc460116b4e8bbb33657538db5374",
+    "url": "/jsonplaceholder-React-Redux/index.html"
+  },
+  {
+    "revision": "c13c1230e1ca9a9743dd",
+    "url": "/jsonplaceholder-React-Redux/static/js/2.22159455.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/jsonplaceholder-React-Redux/static/js/2.22159455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612aaa0c44f7ecc81726",
+    "url": "/jsonplaceholder-React-Redux/static/js/main.c80b997e.chunk.js"
+  },
+  {
+    "revision": "a34c51ac61607a23cf00",
+    "url": "/jsonplaceholder-React-Redux/static/js/runtime-main.455c3387.js"
+  }
+]);
